d5c7bf8398cf612c3f607e569179efa24a4ec6a5
Perke1 <springerjan2@gmail.com>
2023-04-13 18:10:17 +0200

    config change

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -1,21 +1,20 @@
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
-import "firebase/compat/firestore";
+import { getAuth, GithubAuthProvider } from "firebase/auth";
+import { initializeApp } from "firebase/app";
 
 // INPUT YOUR OWN CREDENTIALS https://console.firebase.google.com/u/0/
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBW4HJsD2w4hDVEv0fVp9Q1ip2NEP76X7I",
-  authDomain: "authredux-71e24.firebaseapp.com",
-  projectId: "authredux-71e24",
-  storageBucket: "authredux-71e24.appspot.com",
-  messagingSenderId: "265312777045",
-  appId: "1:265312777045:web:b36023ac6fb422e043b5dc",
-  measurementId: "G-3KZYXS4VZ4",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
 
-export const github = new firebase.auth.GithubAuthProvider();
+export const github = new GithubAuthProvider();
 
-export const auth = firebase.auth();
+export const auth = getAuth();
