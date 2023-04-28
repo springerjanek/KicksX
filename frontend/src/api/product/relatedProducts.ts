14d23e77a3bfb81086638843758055ad7d49f293
Perke1 <springerjan2@gmail.com>
2023-04-28 16:16:28 +0200

    refactor: optimized data fetching

--- frontend/src/api/product/relatedProducts.ts
+++ frontend/src/api/product/relatedProducts.ts
@@ -1,36 +1,34 @@
-import { useQuery } from "react-query";
-import axios from "axios";
+import { useQuery, useQueryClient } from "react-query";
 import { getLowestAskAndHighestBid } from "hooks/getLowestAskAndHighestBid";
-
-const BASE_URL = "http://localhost:3001";
 
 export const useGetRelatedProducts = (
   originalProductName: string,
   combinedFilter: string
 ) => {
+  const queryClient = useQueryClient();
   const { isLoading, error, data } = useQuery({
     queryKey: ["relatedProducts"],
     refetchOnWindowFocus: false,
     queryFn: async () => {
-      const response = await axios.get(BASE_URL);
-      const data: Products = response.data;
+      const data: Products | undefined = queryClient.getQueryData("products");
+      if (data) {
+        const result = await Promise.all(
+          data
+            .filter(
+              (product) =>
+                product.name.includes(combinedFilter) &&
+                product.name !== originalProductName
+            )
+            .map(async (relatedProduct) => {
+              const { id, name, thumbnail } = relatedProduct;
+              const data = await getLowestAskAndHighestBid(relatedProduct);
 
-      const result = await Promise.all(
-        data
-          .filter(
-            (product) =>
-              product.name.includes(combinedFilter) &&
-              product.name !== originalProductName
-          )
-          .map(async (relatedProduct) => {
-            const { id, name, thumbnail } = relatedProduct;
-            const data = await getLowestAskAndHighestBid(relatedProduct);
-
-            let lowestAsk = data[0];
-            return { id, name, thumbnail, lowestAsk };
-          })
-      );
-      return result;
+              let lowestAsk = data[0];
+              return { id, name, thumbnail, lowestAsk };
+            })
+        );
+        return result;
+      }
     },
   });
   return { isLoading, data };
