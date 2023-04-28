33e3e28a16b766e161148f43d8f0b9978c7fd320
Perke1 <springerjan2@gmail.com>
2023-04-28 16:15:40 +0200

    refactor: changed hook name and added stale time to useGetProducts

--- frontend/src/hooks/useGetProducts.ts
+++ frontend/src/hooks/useGetProducts.ts
@@ -5,6 +5,7 @@ export const useGetProducts = () => {
   const { isLoading, data } = useQuery({
     queryKey: ["products"],
     refetchOnWindowFocus: false,
+    staleTime: 1000 * 3600,
     queryFn: async () => {
       const response = await axios.get("http://localhost:3001/");
       const result: Products = response.data;
