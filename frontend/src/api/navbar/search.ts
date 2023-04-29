bf929f30fa581de3aac564084ff78b9354fd417f
Perke1 <springerjan2@gmail.com>
2023-04-29 17:25:48 +0200

    refactor: New search logic

--- frontend/src/api/navbar/search.ts
+++ frontend/src/api/navbar/search.ts
@@ -1,15 +1,12 @@
 export const search = (data: Products, input: string) => {
-  console.log("essa", input);
   const matches: Product[] = [];
-  const rest: Product[] = [];
 
   data!.forEach((product) => {
     const formattedProductTitle = product.name.toLowerCase();
-    formattedProductTitle.includes(input.toLowerCase())
-      ? matches.push(product)
-      : rest.push(product);
+    console.log(formattedProductTitle.includes(input.toLowerCase()));
+    formattedProductTitle.includes(input.toLowerCase()) &&
+      matches.push(product);
   });
-  matches.push(...rest);
 
   return matches;
 };
