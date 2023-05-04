f2de33d40688be756fabca57d31761c9f88fdab4
Perke1 <springerjan2@gmail.com>
2023-05-04 16:49:02 +0200

    refactor: optimized hooks

--- frontend/src/hooks/useGetProductByName.ts
+++ frontend/src/hooks/useGetProductByName.ts
@@ -1,16 +1,27 @@
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient } from "react-query";
 import axios from "axios";
 
 const BASE_URL = "http://localhost:3001";
 
 export const useGetProductByName = (path: string, key: string) => {
+  const queryClient = useQueryClient();
+  const productsData: Products | undefined =
+    queryClient.getQueryData("products");
   const { isLoading, data } = useQuery({
     queryKey: [key],
     refetchOnWindowFocus: false,
     queryFn: async () => {
-      const response = await axios.get(`${BASE_URL + path}`);
-      const result: Product = response.data[0];
-      return result;
+      if (productsData !== undefined) {
+        const productName = path.split("=")[1];
+        const desiredProduct = productsData.filter(
+          (product) => product.name === productName
+        );
+        return desiredProduct[0];
+      } else {
+        const response = await axios.get(`${BASE_URL + path}`);
+        const result: Product = response.data[0];
+        return result;
+      }
     },
   });
 
--- frontend/src/hooks/useGetProducts.ts
+++ frontend/src/hooks/useGetProducts.ts
@@ -5,7 +5,6 @@ export const useGetProducts = () => {
   const { isLoading, data } = useQuery({
     queryKey: ["products"],
     refetchOnWindowFocus: false,
-    staleTime: 1000 * 3600,
     queryFn: async () => {
       const response = await axios.get("http://localhost:3001/");
       const result: Products = response.data;
